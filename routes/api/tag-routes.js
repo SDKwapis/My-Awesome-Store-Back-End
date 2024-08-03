b9c95d1ab72132e874bde5250d6eb94923cb8734
SDKwapis <165666421+SDKwapis@users.noreply.github.com>
2024-08-03 15:50:44 -0400

    added more product routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -4,7 +4,7 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   try {
     const tagData = await Tag.findAll({
-      include: [{ model: Product }],
+      include: [{ model: Product }] //, { model: ProductTag}],
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -16,7 +16,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [{ model: Product }],
+      include: [{ model: Product }] //, { model: ProductTag}],
     });
     if (!tagData) {
       res.status(404).json({ message: "No tag found with that id!" });
